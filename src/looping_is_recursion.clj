9623ba4401c3ba03e8d2e45eaeccd18d06d690ca
Julian Jelfs <julian.jelfs@gmail.com>
2014-12-29 21:26:30 +0000

    trying out some plugins

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -3,27 +3,26 @@
 (defn power [base exp]
   (loop [acc 1
          n exp]
-     (if (zero? n)
-        acc
-        (recur (* acc base) (dec n)))))
+    (if (zero? n)
+      (recur (* acc base) (dec n)))))
 
 (defn last-element [a-seq]
   (loop [sq a-seq]
-     (cond
-       (empty? sq) nil
-       (= (count sq) 1) (first sq)
-       :else
-        (recur (rest sq)))))
+    (cond
+      (empty? sq) nil
+      (= (count sq) 1) (first sq)
+      :else
+      (recur (rest sq)))))
 
 (defn seq= [seq1 seq2]
   (loop [s1 seq1
          s2 seq2]
-       (cond
-         (and (empty? s1) (empty? s2)) true
-         (or (empty? s1) (empty? s2)) false
-         (= (first s1) (first s2))
-            (recur (rest s1) (rest s2))
-         :else false)))
+    (cond
+      (and (empty? s1) (empty? s2)) true
+      (or (empty? s1) (empty? s2)) false
+      (= (first s1) (first s2))
+      (recur (rest s1) (rest s2))
+      :else false)))
 
 (defn find-first-index [pred a-seq]
   (loop [i 0
@@ -32,7 +31,7 @@
       (empty? sq) nil
       (pred (first sq)) i
       :else
-        (recur (inc i) (rest sq))))) 
+      (recur (inc i) (rest sq))))) 
 
 (defn avg [a-seq]
   -1)
